4105f29be26ab49deca1036367031a2061613d75
Nithuzzzz <n71978204@gmail.com>
2026-03-11 23:06:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 let cart = [];
 
-function addToCart(product){
-cart.push(product);
-alert(product + " added to cart");
-console.log(cart);
+function addToCart(name, price){
+
+cart.push({name, price});
+
+updateCartCount();
+
+alert(name + " added to cart");
 }
+
+function updateCartCount(){
+document.getElementById("cart-count").innerText = cart.length;
+}
+
+/* PRODUCT SEARCH */
+
+document.getElementById("searchInput").addEventListener("keyup", function(){
+
+let value = this.value.toLowerCase();
+
+let products = document.querySelectorAll(".product");
+
+products.forEach(function(product){
+
+let name = product.querySelector("h3").innerText.toLowerCase();
+
+if(name.includes(value)){
+product.style.display = "block";
+}else{
+product.style.display = "none";
+}
+
+});
+
+});
